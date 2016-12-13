3f5c7644edb6f8152f942f392ec45f84b847cef1
Chris Mariani <marianic0@students.rowan.edu>
2016-12-12 22:21:38 -0500

    fixed search-partial for accounts page, and started working on new tabs layout slightly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110012343) do
+ActiveRecord::Schema.define(version: 20161213024033) do
 
   create_table "pics", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20161110012343) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_pics_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
